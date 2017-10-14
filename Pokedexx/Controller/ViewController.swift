9b9255611914f7dca3353886941a2f2205292dce
Dhruv Vaghela <dhruvvaghela@Dhruvs-MacBook-Pro.local>
2017-10-15 02:13:32 +0530

    Added custom fonts and music

--- Pokedexx/Controller/ViewController.swift
+++ Pokedexx/Controller/ViewController.swift
@@ -7,18 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var collection: UICollectionView!
     
     var pokemon = [Pokemon]()
+    var musicPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         collection.dataSource = self
         collection.delegate = self
         
         parsePokemonCSV()
+        initAudio()
+    }
+    
+    func initAudio() {
+        let path = Bundle.main.path(forResource: "music", ofType: "mp3")!
+        
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+            
+        } catch let err as NSError {
+                print(err.debugDescription)
+        }
     }
     
     func parsePokemonCSV() {
@@ -74,7 +91,16 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
 
     
-
+    @IBAction func musicBtnPressed(_ sender: UIButton) {
+        if musicPlayer.isPlaying {
+            musicPlayer.pause()
+            sender.alpha = 0.5
+        } else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
+    }
+    
     
 
 }
